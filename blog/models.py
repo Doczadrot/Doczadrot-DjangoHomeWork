e1c8947109cb7a5e1456bc20b2b22a033bf310bc
Dockzadrot <155732866+Doczadrot@users.noreply.github.com>
2025-06-30 17:19:09 +0300

    Реализован полный CRUD (Create, Read, Update, Delete) для новой модели блоговых записей.
    Все контроллеры приложения catalog переведены с Function-Based Views (FBV) на Class-Based Views (CBV).
    
    Добавлена дополнительная логика для улучшения работы блога (счетчик просмотров, фильтрация опубликованных статей,

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.CharField(max_length=255, verbose_name="Slug", unique=True, blank=True, null=True)
+    contents = models.TextField(verbose_name="Содержимое", blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    preview = models.ImageField(upload_to="blog_images/", verbose_name="Изображение", blank=True, null=True)
+    on_published = models.BooleanField(default=False, verbose_name='Опубликовано')
+    count_views = models.IntegerField(default=0, verbose_name='Количество просмотров' )
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "Блог"
+        verbose_name_plural = "Блоги"
+        ordering = ["title"]
