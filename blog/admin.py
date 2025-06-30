9d63f4029706dc73a8327bc651f8aa8409f2dc0d
Dockzadrot <155732866+Doczadrot@users.noreply.github.com>
2025-06-30 17:19:09 +0300

    Реализован полный CRUD (Create, Read, Update, Delete) для новой модели блоговых записей.
    Все контроллеры приложения catalog переведены с Function-Based Views (FBV) на Class-Based Views (CBV).
    
    Добавлена дополнительная логика для улучшения работы блога (счетчик просмотров, фильтрация опубликованных статей,

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+# blog/admin.py
+
+from django.contrib import admin
+from .models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'contents', 'on_published', 'date_created', 'count_views', 'slug')
+    list_filter = ('on_published', 'date_created',)
+    search_fields = ('title', 'contents')
